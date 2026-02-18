260505b79ffa7c013cf59c1066c4f52ceab8386d
Funkariya <pathani050@gmail.com>
2026-02-18 23:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
 from flask import Flask, render_template, request, send_file
+import os
 from process import convert_to_svg
 
 app = Flask(__name__)
@@ -10,17 +10,15 @@ OUTPUT_FOLDER = "outputs"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         file = request.files["image"]
-
         if not file:
             return "No file uploaded", 400
 
         input_path = os.path.join(UPLOAD_FOLDER, file.filename)
-        output_svg = os.path.join(OUTPUT_FOLDER, "final.svg")
+        output_svg = os.path.join(OUTPUT_FOLDER, "result.svg")
 
         file.save(input_path)
         convert_to_svg(input_path, output_svg)
@@ -29,7 +27,6 @@ def index():
 
     return render_template("index.html")
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
