b31007d94060b044050369925ae1c03f478f937b
Funkariya <pathani050@gmail.com>
2026-02-19 11:49:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, request, send_file
-import os
-import uuid
+import os, uuid
 from process import convert_to_svg
 
 app = Flask(__name__)
 
-UPLOAD_DIR = "uploads"
-OUTPUT_DIR = "outputs"
-
-os.makedirs(UPLOAD_DIR, exist_ok=True)
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+UPLOAD = "uploads"
+OUTPUT = "outputs"
+os.makedirs(UPLOAD, exist_ok=True)
+os.makedirs(OUTPUT, exist_ok=True)
 
 @app.route("/")
 def index():
@@ -17,39 +15,20 @@ def index():
 
 @app.route("/convert", methods=["POST"])
 def convert():
-
-    if "image" not in request.files:
-        return "No file uploaded", 400
-
     img = request.files["image"]
-    if img.filename == "":
-        return "Empty file", 400
-
-    # UI values
     colors = int(request.form.get("colors", 6))
     smooth = float(request.form.get("smooth", 1.0))
-    bg = request.form.get("bg", "remove")
+    remove_bg = request.form.get("bg") == "remove"
 
-    uid = uuid.uuid4().hex
-    input_path = f"{UPLOAD_DIR}/{uid}.png"
-    output_svg = f"{OUTPUT_DIR}/{uid}.svg"
+    uid = str(uuid.uuid4())
+    input_path = f"{UPLOAD}/{uid}.png"
+    output_path = f"{OUTPUT}/{uid}.svg"
 
     img.save(input_path)
 
-    convert_to_svg(
-        input_path,
-        output_svg,
-        colors,
-        smooth,
-        bg == "remove"
-    )
+    convert_to_svg(input_path, output_path, colors, smooth, remove_bg)
 
-    return send_file(
-        output_svg,
-        as_attachment=True,
-        download_name="vector.svg",
-        mimetype="image/svg+xml"
-    )
+    return send_file(output_path, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
